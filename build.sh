be204392f2048ed1db4a557579f7984f0f68c207
Herbert Marshall <herbert@herbmarshall.com>
2025-01-09 21:55:31 -0500

    UTIL-362 Fix build for empty stash

--- build.sh
+++ build.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#
+# This file is part of herbmarshall.com: base.library  ( hereinafter "base.library" ).
+#
+# base.library is free software: you can redistribute it and/or modify it under the terms of
+# the GNU General Public License as published by the Free Software Foundation, either version 2 of the License,
+# or (at your option) any later version.
+#
+# base.library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with base.library.
+# If not, see <https://www.gnu.org/licenses/>.
+#
+
 set -e
 
 # Using non-cdlocal because may be ran by jenkins
@@ -60,7 +74,10 @@ if [[ "build" == "${OPERATION}" ]]; then
 
 	STASH_MESSAGE="Prebuild stash $( date )"
 	if [[ "${STASH}" == 'true' ]]; then
-		git stash save "${STASH_MESSAGE}"
+		if [[ "No local changes to save" == "$( git stash save \"${STASH_MESSAGE}\" )" ]]; then
+			echo "Disabling stash as 'No local changes to save'"
+			STASH=false
+		fi
 	fi
 
 	docker run -it --rm \
