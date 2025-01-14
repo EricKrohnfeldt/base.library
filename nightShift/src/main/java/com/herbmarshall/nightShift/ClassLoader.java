488f214aadb48dcb9ba74867667b651726e25e56
Herbert Marshall <herbert@herbmarshall.com>
2025-01-14 09:53:11 -0500

    UTIL-362 ClassLoader

---
+++ nightShift/src/main/java/com/herbmarshall/nightShift/ClassLoader.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of herbmarshall.com: base.library  ( hereinafter "base.library" ).
+ *
+ * base.library is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * base.library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with base.library.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.herbmarshall.nightShift;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+final class ClassLoader {
+
+	private ClassLoader() {}
+
+	static Function<String, Class<?>> loader() {
+		final var loader = Thread.currentThread().getContextClassLoader();
+		return target -> {
+			try {
+				return loader.loadClass( Objects.requireNonNull( target ) );
+			}
+			catch ( ClassNotFoundException exception ) {
+				throw new RuntimeException(
+					error_badClassName( target ),
+					exception
+				);
+			}
+		};
+	}
+
+	static String error_badClassName( String name ) {
+		return "Cannot load class with name " + name;
+	}
+
+}
---
+++ nightShift/src/test/java/com/herbmarshall/nightShift/ClassLoaderTest.java
@@ -0,0 +1,79 @@
+/*
+ * This file is part of herbmarshall.com: base.library  ( hereinafter "base.library" ).
+ *
+ * base.library is free software: you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation, either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * base.library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with base.library.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.herbmarshall.nightShift;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+import java.util.function.Function;
+
+import static com.herbmarshall.nightShift.ClassLoader.error_badClassName;
+import static com.herbmarshall.nightShift.ClassLoader.loader;
+
+final class ClassLoaderTest {
+
+	@Test
+	void happyPath() {
+		// Arrange
+		Function<String, Class<?>> loader = loader();
+		Class<?> expected = PackagesForTesting.happyPath
+			.getAutomatedClasses()
+			.findFirst()
+			.orElseThrow();
+		String className = expected.getCanonicalName();
+		// Act
+		Class<?> output = loader.apply( className );
+		// Assert
+		Assertions.assertSame( expected, output );
+	}
+
+	@Test
+	void classNotFound() {
+		// Arrange
+		Function<String, Class<?>> loader = loader();
+		String className = randomString();
+		// Act
+		try {
+			loader.apply( className );
+		}
+		// Assert
+		catch ( RuntimeException exception ) {
+			Assertions.assertEquals(
+				error_badClassName( className ),
+				exception.getMessage()
+			);
+		}
+	}
+
+	@Test
+	void null_target() {
+		// Arrange
+		Function<String, Class<?>> loader = loader();
+		// Act
+		try {
+			loader.apply( null );
+		}
+		// Assert
+		catch ( NullPointerException npe ) {
+			System.out.println( "Pass: " + npe.getMessage() );
+		}
+	}
+
+	private static String randomString() {
+		return UUID.randomUUID().toString();
+	}
+
+}
